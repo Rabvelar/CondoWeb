7d512fb85a353d187851cede7dd3b908c0eb4b0a
Rabvelar <156091755+Rabvelar@users.noreply.github.com>
2024-11-01 20:30:50 +0700

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -13,7 +13,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 DEBUG=True
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Application definition
 INSTALLED_APPS = [
